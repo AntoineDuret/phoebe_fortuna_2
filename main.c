237feff909ee940a830ca3381c57bace0db0022e
duret <antoine.duret@epfl.ch>
2021-05-14 01:26:53 +0200

    Test 2: 4 mics + éléments inutiles supprimés

--- main.c
+++ main.c
@@ -65,7 +65,7 @@ int main(void) {
 	obstacle_det_start();
 	VL53L0X_start();			// ToF init
     mic_start(&process_audio_data);
-    mic_input_start();
+//    mic_input_start();
     process_image_start();
 
     uint8_t nbPlayers = 0;
