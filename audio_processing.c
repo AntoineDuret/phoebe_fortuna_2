9a3c93e4c99240445702fc9c3c632c62bf52b579
duret <antoine.duret@epfl.ch>
2021-05-14 01:26:53 +0200

    Test 2: 4 mics + éléments inutiles supprimés

--- audio_processing.c
+++ audio_processing.c
@@ -2,7 +2,7 @@
   \file   	audio_processing.c
   \author 	Antoine Duret & Carla Schmid (Group G08)
   \date   	13.05.2021
-  \version	2.0
+  \version	2.1
   \brief  	Code for audio processing related tasks
 */
 
@@ -34,45 +34,53 @@ static BSEMAPHORE_DECL(sendToComputer_sem, TRUE); // @suppress("Field cannot be 
 
 // 2 times FFT_SIZE because these arrays contain complex numbers (real + imaginary)
 static float micLeft_cmplx_input[2 * FFT_SIZE];
-//static float micRight_cmplx_input[2 * FFT_SIZE];
-//static float micFront_cmplx_input[2 * FFT_SIZE];
-//static float micBack_cmplx_input[2 * FFT_SIZE];
+static float micRight_cmplx_input[2 * FFT_SIZE];
+static float micFront_cmplx_input[2 * FFT_SIZE];
+static float micBack_cmplx_input[2 * FFT_SIZE];
 
 // Arrays containing the computed magnitude of the complex numbers
 static float micLeft_output[FFT_SIZE];
-//static float micRight_output[FFT_SIZE];
-//static float micFront_output[FFT_SIZE];
-//static float micBack_output[FFT_SIZE];
+static float micRight_output[FFT_SIZE];
+static float micFront_output[FFT_SIZE];
+static float micBack_output[FFT_SIZE];
+static float four_mics_output[FFT_SIZE];	// for the average magnitude
 
 static bool audio_command_on = 0;
 static bool voice_calibration_on = 0;
 static uint8_t mid_freq = MID_FREQ;
 
 
-// Thread for microphone input
-static THD_WORKING_AREA(micInput_thd_wa, 2048);
-static THD_FUNCTION(micInput_thd, arg) {
-    (void)arg;
-    chRegSetThreadName(__FUNCTION__);
-
-    while(1) {
-      	float* bufferCmplxInput = get_audio_buffer_ptr(LEFT_CMPLX_INPUT);
-        float* bufferOutput = get_audio_buffer_ptr(LEFT_OUTPUT);
-
-        uint16_t size = receive_int16_from_computer((BaseSequentialStream *) &SD3,
-        													bufferCmplxInput, FFT_SIZE);
-
-        if(size == FFT_SIZE) {
-            /*   Optimized FFT   */
-        	chSysLock();
-            doFFT_optimized(FFT_SIZE, bufferCmplxInput);
-            arm_cmplx_mag_f32(bufferCmplxInput, bufferOutput, FFT_SIZE);
-            chSysUnlock();
-        }
-
-        chThdSleepMilliseconds(100); // sensors 100 Hz, PID 10Hz,
-    }
-}
+//// Thread for microphone input
+//static THD_WORKING_AREA(micInput_thd_wa, 2048);
+//static THD_FUNCTION(micInput_thd, arg) {
+//    (void)arg;
+//    chRegSetThreadName(__FUNCTION__);
+//
+//    while(1) {
+//      	float* bufferCmplxInput = get_audio_buffer_ptr(LEFT_CMPLX_INPUT);
+////      						get_audio_buffer_ptr(RIGHT_CMPLX_INPUT)
+////								get_audio_buffer_ptr(FRONT_CMPLX_INPUT)
+////								get_audio_buffer_ptr(BACK_CMPLX_INPUT)
+//
+//        float* bufferOutput = get_audio_buffer_ptr(LEFT_OUTPUT);
+////      						get_audio_buffer_ptr(RIGHT_OUTPUT)
+////								get_audio_buffer_ptr(FRONT_OUTPUT)
+////								get_audio_buffer_ptr(BACK_OUTPUT)
+//
+//        uint16_t size = receive_int16_from_computer((BaseSequentialStream *) &SD3,
+//        													bufferCmplxInput, FFT_SIZE);
+//
+//        if(size == FFT_SIZE) {
+//            /*   Optimized FFT   */
+//        	chSysLock();
+//            doFFT_optimized(FFT_SIZE, bufferCmplxInput);
+//            arm_cmplx_mag_f32(bufferCmplxInput, bufferOutput, FFT_SIZE);
+//            chSysUnlock();
+//        }
+//
+//        chThdSleepMilliseconds(100); // sensors 100 Hz, PID 10Hz,
+//    }
+//}
 
 
 /*
@@ -94,17 +102,17 @@ void process_audio_data(int16_t *data, uint16_t num_samples) {
 	// Loop to fill the buffers
 	for(uint16_t i = 0 ; i < num_samples ; i+=4) {
 		// Construct an array of complex numbers. Put 0 to the imaginary part
-		//micRight_cmplx_input[nb_samples] = (float)data[i + MIC_RIGHT];
+		micRight_cmplx_input[nb_samples] = (float)data[i + MIC_RIGHT];
 		micLeft_cmplx_input[nb_samples] = (float)data[i + MIC_LEFT];
-		//micBack_cmplx_input[nb_samples] = (float)data[i + MIC_BACK];
-		//micFront_cmplx_input[nb_samples] = (float)data[i + MIC_FRONT];
+		micBack_cmplx_input[nb_samples] = (float)data[i + MIC_BACK];
+		micFront_cmplx_input[nb_samples] = (float)data[i + MIC_FRONT];
 
 		nb_samples++;
 
-		//micRight_cmplx_input[nb_samples] = 0;
+		micRight_cmplx_input[nb_samples] = 0;
 		micLeft_cmplx_input[nb_samples] = 0;
-		//micBack_cmplx_input[nb_samples] = 0;
-		//micFront_cmplx_input[nb_samples] = 0;
+		micBack_cmplx_input[nb_samples] = 0;
+		micFront_cmplx_input[nb_samples] = 0;
 
 		nb_samples++;
 
@@ -122,9 +130,9 @@ void process_audio_data(int16_t *data, uint16_t num_samples) {
 		*	We use only the left microphone.
 		*/
 		doFFT_optimized(FFT_SIZE, micLeft_cmplx_input);
-		//doFFT_optimized(FFT_SIZE, micRight_cmplx_input);
-		//doFFT_optimized(FFT_SIZE, micBack_cmplx_input);
-		//doFFT_optimized(FFT_SIZE, micFront_cmplx_input);
+		doFFT_optimized(FFT_SIZE, micRight_cmplx_input);
+		doFFT_optimized(FFT_SIZE, micBack_cmplx_input);
+		doFFT_optimized(FFT_SIZE, micFront_cmplx_input);
 
 		/*
 		*	- Magnitude processing -
@@ -132,20 +140,25 @@ void process_audio_data(int16_t *data, uint16_t num_samples) {
 		*	in a buffer of FFT_SIZE because it only contains real numbers.
 		*/
 		arm_cmplx_mag_f32(micLeft_cmplx_input, micLeft_output, FFT_SIZE);
-		//arm_cmplx_mag_f32(micRight_cmplx_input, micRight_output, FFT_SIZE);
-		//arm_cmplx_mag_f32(micBack_cmplx_input, micBack_output, FFT_SIZE);
-		//arm_cmplx_mag_f32(micFront_cmplx_input, micFront_output, FFT_SIZE);
+		arm_cmplx_mag_f32(micRight_cmplx_input, micRight_output, FFT_SIZE);
+		arm_cmplx_mag_f32(micBack_cmplx_input, micBack_output, FFT_SIZE);
+		arm_cmplx_mag_f32(micFront_cmplx_input, micFront_output, FFT_SIZE);
 
 
-		// Sends only one FFT result over 8 for 1 mic
+		// FFT result for 4 mics
 		if((voice_calibration_on)) {
-			player_voice_calibration(micLeft_output);
+			for(int16_t i = 0; i < FFT_SIZE; i++) {
+				four_mics_output[i] = (micLeft_output[i] + micRight_output[i]
+											+ micBack_output[i] + micFront_output[i])/4;
+			}
+
+			player_voice_calibration(four_mics_output);
 		}
 
 		nb_samples = 0;
 
 		if(audio_command_on) {
-			sound_remote(micLeft_output);
+			sound_remote(four_mics_output);
 		}
 	}
 }
@@ -162,12 +175,12 @@ void doFFT_optimized(uint16_t size, float* complex_buffer) {
 }
 
 
-/*
-*	Simple function to put the invoking thread into sleep until it can process the audio data
-*/
-void wait_send_to_computer(void){
-	chBSemWait(&sendToComputer_sem);
-}
+///*
+//*	Simple function to put the invoking thread into sleep until it can process the audio data
+//*/
+//void wait_send_to_computer(void){
+//	chBSemWait(&sendToComputer_sem);
+//}
 
 
 /*
@@ -177,28 +190,27 @@ float* get_audio_buffer_ptr(BUFFER_NAME_t name) {
 	if(name == LEFT_CMPLX_INPUT) {
 		return micLeft_cmplx_input;
 	}
-//	}
-//	else if(name == RIGHT_CMPLX_INPUT) {
-//		return micRight_cmplx_input;
-//	}
-//	else if(name == FRONT_CMPLX_INPUT) {
-//		return micFront_cmplx_input;
-//	}
-//	else if(name == BACK_CMPLX_INPUT) {
-//		return micBack_cmplx_input;
-//	}
+	else if(name == RIGHT_CMPLX_INPUT) {
+		return micRight_cmplx_input;
+	}
+	else if(name == FRONT_CMPLX_INPUT) {
+		return micFront_cmplx_input;
+	}
+	else if(name == BACK_CMPLX_INPUT) {
+		return micBack_cmplx_input;
+	}
 	else if(name == LEFT_OUTPUT) {
 		return micLeft_output;
 	}
-//	else if(name == RIGHT_OUTPUT) {
-//		return micRight_output;
-//	}
-//	else if(name == FRONT_OUTPUT) {
-//		return micFront_output;
-//	}
-//	else if(name == BACK_OUTPUT) {
-//		return micBack_output;
-//	}
+	else if(name == RIGHT_OUTPUT) {
+		return micRight_output;
+	}
+	else if(name == FRONT_OUTPUT) {
+		return micFront_output;
+	}
+	else if(name == BACK_OUTPUT) {
+		return micBack_output;
+	}
 	else {
 		return NULL;
 	}
@@ -210,12 +222,12 @@ float* get_audio_buffer_ptr(BUFFER_NAME_t name) {
 /* 									 NEW FUNCTIONS										*/
 /*======================================================================================*/
 
-/*
-*	Function to start the THREAD controlling the mic.
-*/
-void mic_input_start(void) {
-	chThdCreateStatic(micInput_thd_wa, sizeof(micInput_thd_wa), NORMALPRIO, micInput_thd, NULL);
-}
+///*
+//*	Function to start the THREAD controlling the mic.
+//*/
+//void mic_input_start(void) {
+//	chThdCreateStatic(micInput_thd_wa, sizeof(micInput_thd_wa), NORMALPRIO, micInput_thd, NULL);
+//}
 
 
 /*
