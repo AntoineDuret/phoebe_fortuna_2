223d143b98f9dda26ee44aacc49248de062c2b0b
duret <antoine.duret@epfl.ch>
2021-05-14 21:17:16 +0200

    Version correcte 4 mics

--- audio_processing.c
+++ audio_processing.c
@@ -34,15 +34,16 @@ static BSEMAPHORE_DECL(sendToComputer_sem, TRUE); // @suppress("Field cannot be 
 
 // 2 times FFT_SIZE because these arrays contain complex numbers (real + imaginary)
 static float micLeft_cmplx_input[2 * FFT_SIZE];
-//static float micRight_cmplx_input[2 * FFT_SIZE];
-//static float micFront_cmplx_input[2 * FFT_SIZE];
-//static float micBack_cmplx_input[2 * FFT_SIZE];
+static float micRight_cmplx_input[2 * FFT_SIZE];
+static float micFront_cmplx_input[2 * FFT_SIZE];
+static float micBack_cmplx_input[2 * FFT_SIZE];
 
 // Arrays containing the computed magnitude of the complex numbers
 static float micLeft_output[FFT_SIZE];
-//static float micRight_output[FFT_SIZE];
-//static float micFront_output[FFT_SIZE];
-//static float micBack_output[FFT_SIZE];
+static float micRight_output[FFT_SIZE];
+static float micFront_output[FFT_SIZE];
+static float micBack_output[FFT_SIZE];
+static float four_mics_output[FFT_SIZE];
 
 static bool audio_command_on = 0;
 static bool voice_calibration_on = 0;
@@ -68,17 +69,17 @@ void process_audio_data(int16_t *data, uint16_t num_samples) {
 	// Loop to fill the buffers
 	for(uint16_t i = 0 ; i < num_samples ; i+=4) {
 		// Construct an array of complex numbers. Put 0 to the imaginary part
-		//micRight_cmplx_input[nb_samples] = (float)data[i + MIC_RIGHT];
+		micRight_cmplx_input[nb_samples] = (float)data[i + MIC_RIGHT];
 		micLeft_cmplx_input[nb_samples] = (float)data[i + MIC_LEFT];
-		//micBack_cmplx_input[nb_samples] = (float)data[i + MIC_BACK];
-		//micFront_cmplx_input[nb_samples] = (float)data[i + MIC_FRONT];
+		micBack_cmplx_input[nb_samples] = (float)data[i + MIC_BACK];
+		micFront_cmplx_input[nb_samples] = (float)data[i + MIC_FRONT];
 
 		nb_samples++;
 
-		//micRight_cmplx_input[nb_samples] = 0;
+		micRight_cmplx_input[nb_samples] = 0;
 		micLeft_cmplx_input[nb_samples] = 0;
-		//micBack_cmplx_input[nb_samples] = 0;
-		//micFront_cmplx_input[nb_samples] = 0;
+		micBack_cmplx_input[nb_samples] = 0;
+		micFront_cmplx_input[nb_samples] = 0;
 
 		nb_samples++;
 
@@ -96,9 +97,9 @@ void process_audio_data(int16_t *data, uint16_t num_samples) {
 		*	We use only the left microphone.
 		*/
 		doFFT_optimized(FFT_SIZE, micLeft_cmplx_input);
-		//doFFT_optimized(FFT_SIZE, micRight_cmplx_input);
-		//doFFT_optimized(FFT_SIZE, micBack_cmplx_input);
-		//doFFT_optimized(FFT_SIZE, micFront_cmplx_input);
+		doFFT_optimized(FFT_SIZE, micRight_cmplx_input);
+		doFFT_optimized(FFT_SIZE, micBack_cmplx_input);
+		doFFT_optimized(FFT_SIZE, micFront_cmplx_input);
 
 		/*
 		*	- Magnitude processing -
@@ -106,20 +107,29 @@ void process_audio_data(int16_t *data, uint16_t num_samples) {
 		*	in a buffer of FFT_SIZE because it only contains real numbers.
 		*/
 		arm_cmplx_mag_f32(micLeft_cmplx_input, micLeft_output, FFT_SIZE);
-		//arm_cmplx_mag_f32(micRight_cmplx_input, micRight_output, FFT_SIZE);
-		//arm_cmplx_mag_f32(micBack_cmplx_input, micBack_output, FFT_SIZE);
-		//arm_cmplx_mag_f32(micFront_cmplx_input, micFront_output, FFT_SIZE);
+		arm_cmplx_mag_f32(micRight_cmplx_input, micRight_output, FFT_SIZE);
+		arm_cmplx_mag_f32(micBack_cmplx_input, micBack_output, FFT_SIZE);
+		arm_cmplx_mag_f32(micFront_cmplx_input, micFront_output, FFT_SIZE);
 
 
-		// Sends only one FFT result over 8 for 1 mic
 		if((voice_calibration_on)) {
-			player_voice_calibration(micLeft_output);
+			for(uint16_t i = 0; i < FFT_SIZE; i++) {
+				four_mics_output[i] = (micLeft_output[i] + micRight_output[i]
+										+ micFront_output[i] + micBack_output[i])/4.0;
+			}
+
+			player_voice_calibration(four_mics_output);
 		}
 
 		nb_samples = 0;
 
 		if(audio_command_on) {
-			sound_remote(micLeft_output);
+			for(uint16_t i = 0; i < FFT_SIZE; i++) {
+				four_mics_output[i] = (micLeft_output[i] + micRight_output[i]
+										+ micFront_output[i] + micBack_output[i])/4.0;
+			}
+
+			sound_remote(four_mics_output);
 		}
 	}
 }
